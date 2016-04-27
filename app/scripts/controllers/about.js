2df5664de9b53dd69d66a6b734171b688438b9c3
Andreas M <andreas@amkreativ.se>
2016-04-27 23:45:52 +0200

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name antstatApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the antstatApp
+ */
+angular.module('antstatApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name antstatApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the antstatApp
+ */
+angular.module('antstatApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
